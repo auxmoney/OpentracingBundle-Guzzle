a7ffab49c2e0e834627faa6350255dfbb4391c8c
Christian Alexander Wolf <mail@cawolf.de>
2020-01-07 15:34:54 +0100

    add extension

---
+++ DependencyInjection/OpentracingGuzzleExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Auxmoney\OpentracingBundleGuzzle\DependencyInjection;
+
+use Exception;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class OpentracingGuzzleExtension extends Extension
+{
+    /**
+     * @throws Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
+}

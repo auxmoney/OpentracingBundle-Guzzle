35c5698e22ef1e06dd616e38756ae7f777a46cfa
Christian Alexander Wolf <mail@cawolf.de>
2020-01-24 16:18:06 +0100

    feature: add request spanning

---
+++ Tests/Middleware/GuzzleRequestSpanningTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Auxmoney\OpentracingBundleGuzzle\Tests\Middleware;
+
+use Auxmoney\OpentracingBundle\Internal\Decorator\RequestSpanning;
+use Auxmoney\OpentracingBundle\Service\Tracing;
+use Auxmoney\OpentracingBundleGuzzle\Middleware\GuzzleRequestSpanning;
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Promise\Promise;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+
+class GuzzleRequestSpanningTest extends TestCase
+{
+    private $requestSpanning;
+    private $tracing;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->requestSpanning = $this->prophesize(RequestSpanning::class);
+        $this->tracing = $this->prophesize(Tracing::class);
+    }
+
+    public function test__invoke(): void
+    {
+        $this->requestSpanning->start('GET', '/foo-uri')->shouldBeCalled();
+        $this->requestSpanning->finish(201)->shouldBeCalled();
+        $this->tracing->finishActiveSpan()->shouldBeCalled();
+
+        $subject = new GuzzleRequestSpanning($this->requestSpanning->reveal(), $this->tracing->reveal());
+
+        $handler = $subject(function () { // function($request, $options)
+            return new FulfilledPromise(new Response(201, [], 'some body'));
+        });
+
+        /** @var Promise $promise */
+        $promise = $handler(new Request('GET', '/foo-uri'), []);
+        /** @var ResponseInterface $response */
+        $response = $promise->wait();
+
+        self::assertSame('some body', $response->getBody()->getContents());
+        self::assertSame(201, $response->getStatusCode());
+    }
+}

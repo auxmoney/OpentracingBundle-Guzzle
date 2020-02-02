016c89054cffc0cbe640b9462cd2228e9ba2f811
Christian Alexander Wolf <mail@cawolf.de>
2020-02-02 13:55:59 +0100

    fix: ps auxing

--- Tests/Functional/teardown.sh
+++ Tests/Functional/teardown.sh
@@ -3,6 +3,9 @@
 cd build/testproject
 symfony local:server:stop
 symfony local:server:list
+ps aux | grep php
+ps aux | grep symfony
+ps aux | grep docker
 cd ../../
 rm -fr build/testproject
-exit 0
+return 0

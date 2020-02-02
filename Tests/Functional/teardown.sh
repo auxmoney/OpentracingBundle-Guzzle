e24f1bcd96ea9453d1bd70a01df3b6fb1a64c946
Christian Alexander Wolf <mail@cawolf.de>
2020-02-02 14:29:33 +0100

    fix: manually stopping fpm

--- Tests/Functional/teardown.sh
+++ Tests/Functional/teardown.sh
@@ -2,10 +2,7 @@
 
 cd build/testproject
 symfony local:server:stop
-symfony local:server:list
-ps aux | grep php
-ps aux | grep symfony
-ps aux | grep docker
 cd ../../
 rm -fr build/testproject
-return 0
+systemctl stop php-fpm
+exit 0

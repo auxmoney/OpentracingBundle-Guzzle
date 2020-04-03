1ccfbd76cfc38ecbe987958ace1016eeb4a0ca11
charles-edouard.breteche <charles.breteche@agriconomie.com>
2020-04-03 19:26:39 +0200

    add guzzle promise onRejected handler

--- Middleware/GuzzleRequestSpanning.php
+++ Middleware/GuzzleRequestSpanning.php
@@ -6,6 +6,7 @@ namespace Auxmoney\OpentracingGuzzleBundle\Middleware;
 
 use Auxmoney\OpentracingBundle\Internal\Decorator\RequestSpanning;
 use Auxmoney\OpentracingBundle\Service\Tracing;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Promise\PromiseInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
@@ -30,11 +31,37 @@ final class GuzzleRequestSpanning
             $promise = $handler($request, $options);
             return $promise->then(
                 function (ResponseInterface $response) {
-                    $this->requestSpanning->finish($response->getStatusCode());
-                    $this->tracing->finishActiveSpan();
+                    $this->onFulfilled($response);
                     return $response;
+                },
+                function (RequestException $exception) {
+                    $this->onRejected($exception);
+                    throw $exception;
                 }
             );
         };
+    }
+
+    private function onFulfilled(ResponseInterface $response): void
+    {
+        $this->requestSpanning->finish($response->getStatusCode());
+        $this->tracing->finishActiveSpan();
+    }
+
+    private function onRejected(RequestException $exception): void
+    {
+        $this->tracing->logInActiveSpan(
+            [
+                'event' => 'error',
+                'error.kind' => 'Exception',
+                'error.object' => get_class($exception),
+                'message' => $exception->getMessage(),
+                'stack' => $exception->getTraceAsString(),
+            ]
+        );
+        if ($exception->hasResponse()) {
+            $this->requestSpanning->finish($exception->getResponse()->getStatusCode());
+        }
+        $this->tracing->finishActiveSpan();
     }
 }

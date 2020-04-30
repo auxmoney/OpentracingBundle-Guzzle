4b6c93cd64077598c090a703e301401e16f6d0df
Christian Alexander Wolf <cawolf@users.noreply.github.com>
2020-04-30 07:52:28 +0200

    fix: allow functionals from forks (#19)
    contributors: @cHeeSaW

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd build/testproject/
+composer config repositories.origin vcs https://github.com/${PR_ORIGIN}
 composer require auxmoney/opentracing-bundle-guzzle:dev-${BRANCH}
 cd ../../

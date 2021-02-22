9408b74c64361a7c207d64235a4537e163bbb951
Filis Futsarov <filisko@users.noreply.github.com>
2021-02-22 10:55:00 +0100

    test: add test to Tests/Middleware/GuzzleRequestSpanningTest.php (#26)
    contributors: @filisko

--- Tests/Middleware/GuzzleRequestSpanningTest.php
+++ Tests/Middleware/GuzzleRequestSpanningTest.php
@@ -55,22 +55,63 @@ class GuzzleRequestSpanningTest extends TestCase
     {
         $this->expectException(RequestException::class);
 
+        $request = new Request('GET', '/foo-uri');
+        $exception =  new RequestException(
+            'An error occured',
+            $request
+        );
+
         $this->requestSpanning->start('GET', '/foo-uri')->shouldBeCalled();
         $this->requestSpanning->finish(Argument::any())->shouldNotBeCalled();
-        $this->tracing->logInActiveSpan(Argument::any())->shouldBeCalled();
+        $this->tracing->logInActiveSpan(Argument::is([
+            'event' => 'error',
+            'error.kind' => 'Exception',
+            'error.object' => RequestException::class,
+            'message' => $exception->getMessage(),
+            'stack' => $exception->getTraceAsString(),
+        ]))->shouldBeCalled();
+        $this->tracing->setTagOfActiveSpan('auxmoney-opentracing-bundle.span-origin', 'guzzle:request')->shouldBeCalled();
+        $this->tracing->finishActiveSpan()->shouldBeCalled();
+
+        $subject = new GuzzleRequestSpanning($this->requestSpanning->reveal(), $this->tracing->reveal());
+
+        $handler = $subject(function () use ($exception) { // function($request, $options)
+            return new RejectedPromise($exception);
+        });
+
+        /** @var Promise $promise */
+        $promise = $handler($request, []);
+        $promise->wait();
+    }
+
+    public function test__invokeRejectedWithAnExceptionThatContainsResponse(): void
+    {
+        $this->expectException(RequestException::class);
+
+        $request = new Request('GET', '/foo-uri');
+        $exception =  new RequestException(
+            'An error occured',
+            $request,
+            new Response(503)
+        );
+
+        $this->requestSpanning->start('GET', '/foo-uri')->shouldBeCalled();
+        $this->requestSpanning->finish(Argument::is(503))->shouldBeCalled();
+        $this->tracing->logInActiveSpan(Argument::is([
+            'event' => 'error',
+            'error.kind' => 'Exception',
+            'error.object' => RequestException::class,
+            'message' => $exception->getMessage(),
+            'stack' => $exception->getTraceAsString(),
+        ]))->shouldBeCalled();
         $this->tracing->setTagOfActiveSpan('auxmoney-opentracing-bundle.span-origin', 'guzzle:request')->shouldBeCalled();
         $this->tracing->finishActiveSpan()->shouldBeCalled();
 
         $subject = new GuzzleRequestSpanning($this->requestSpanning->reveal(), $this->tracing->reveal());
         $request = new Request('GET', '/foo-uri');
 
-        $handler = $subject(function () use ($request) { // function($request, $options)
-            return new RejectedPromise(
-                new RequestException(
-                    'An error occured',
-                    $request
-                )
-            );
+        $handler = $subject(function () use ($exception) { // function($request, $options)
+            return new RejectedPromise($exception);
         });
 
         /** @var Promise $promise */

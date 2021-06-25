ac5e0662802cfc5a195214f6494f6fb4d17f3da5
Christian A. Wolf <mail@cawolf.de>
2021-06-25 07:39:59 +0200

    fix: retrigger actions

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 
 cd build/testproject/
-composer require auxmoney/opentracing-bundle-guzzle:dev-drop-php-71-72
+composer require auxmoney/opentracing-bundle-guzzle:dev-drop-php-71-72 # revert this
 rm -fr vendor/auxmoney/opentracing-bundle-guzzle/*
 cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-guzzle
 composer dump-autoload

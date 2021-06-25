897dcdb6a5ee04d6afadfe47d05c7c3c6c7b4842
Christian A. Wolf <mail@cawolf.de>
2021-06-25 08:17:29 +0200

    fix: use curlhandler instead of mock handler

--- Tests/Functional/TestProjectFiles/existingHandler/src/Service/TestHandler.php
+++ Tests/Functional/TestProjectFiles/existingHandler/src/Service/TestHandler.php
@@ -4,18 +4,8 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-use Psr\Http\Message\RequestInterface;
+use GuzzleHttp\Handler\CurlHandler;
 
-class TestHandler
+class TestHandler extends CurlHandler
 {
-    public function __invoke(): callable
-    {
-        $header = 'a';
-        $value = 'b';
-        return function (
-            RequestInterface $request
-        ) use ($header, $value) {
-            return $request->withHeader($header, $value);
-        };
-    }
 }

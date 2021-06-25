4411e34351e82bc36fe9a090d999ab3aec763dc8
Christian A. Wolf <mail@cawolf.de>
2021-06-25 09:42:36 +0200

    fix: revert temporary dependency version [skip actions]

--- Tests/Functional/Scripts/requireAdditionalVendors.sh
+++ Tests/Functional/Scripts/requireAdditionalVendors.sh
@@ -2,7 +2,7 @@
 shopt -s extglob
 
 cd build/testproject/
-composer require auxmoney/opentracing-bundle-guzzle:dev-drop-php-71-72 # revert this
+composer require auxmoney/opentracing-bundle-guzzle
 rm -fr vendor/auxmoney/opentracing-bundle-guzzle/*
 cp -r ../../!(build|vendor) vendor/auxmoney/opentracing-bundle-guzzle
 composer dump-autoload
